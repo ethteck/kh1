c7d4c0b03c39745bf11fe5557a4ba83c016eee81
Ethan Roseman <ethteck@gmail.com>
2023-09-13 14:07:20 +0900

    git subrepo pull --branch=ps2 tools/splat
    subrepo:
      subdir:   "tools/splat"
      merged:   "dbb6331"
    upstream:
      origin:   "https://github.com/ethteck/splat.git"
      branch:   "ps2"
      commit:   "dbb6331"
    git-subrepo:
      version:  "0.4.5"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "aa416e4"

--- tools/splat/disassembler/spimdisasm_disassembler.py
+++ tools/splat/disassembler/spimdisasm_disassembler.py
@@ -107,6 +107,10 @@ class SpimdisasmDisassembler(disassembler.Disassembler):
             opts.disasm_unknown
         )
 
+        if opts.compiler == compiler.GCC and opts.platform == "ps2":
+            spimdisasm.common.GlobalConfig.DISASSEMBLE_UNKNOWN_INSTRUCTIONS = True
+        rabbitizer.config.toolchainTweaks_treatJAsUnconditionalBranch = False
+
     def check_version(self, skip_version_check: bool, splat_version: str):
         if not skip_version_check and spimdisasm.__version_info__ < self.SPIMDISASM_MIN:
             log.error(
