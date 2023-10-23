475af4babeb1a3290eb1bf8faf375e7712b5a614
Yotona <nzmcqueen1996@gmail.com>
2023-10-23 14:19:45 -0600

    implemented requested changes

--- src/xtrumpet.c
+++ src/xtrumpet.c
@@ -2,68 +2,68 @@
 
 typedef struct XTrumpet {
     /* 0x000 */ u8 unk_00[16];
-    /* 0x010 */ int unk_10;
-    /* 0x014 */ int unk_14;
-    /* 0x018 */ int unk_18;
-    /* 0x01C */ int unk_1c;
-    /* 0x020 */ int unk_20;
-    /* 0x024 */ int unk_24;
-    /* 0x028 */ int unk_28;
-    /* 0x02C */ int unk_2c;
-    /* 0x030 */ int unk_30;
-    /* 0x034 */ int unk_34;
-    /* 0x038 */ int unk_38;
-    /* 0x03C */ int unk_3c;
-    /* 0x040 */ int unk_40;
-    /* 0x044 */ int unk_44;
-    /* 0x048 */ int unk_48;
-    /* 0x04C */ int unk_4c;
+    /* 0x010 */ s32 unk_10;
+    /* 0x014 */ s32 unk_14;
+    /* 0x018 */ s32 unk_18;
+    /* 0x01C */ s32 unk_1C;
+    /* 0x020 */ s32 unk_20;
+    /* 0x024 */ s32 unk_24;
+    /* 0x028 */ s32 unk_28;
+    /* 0x02C */ s32 unk_2C;
+    /* 0x030 */ s32 unk_30;
+    /* 0x034 */ s32 unk_34;
+    /* 0x038 */ s32 unk_38;
+    /* 0x03C */ s32 unk_3C;
+    /* 0x040 */ s32 unk_40;
+    /* 0x044 */ s32 unk_44;
+    /* 0x048 */ s32 unk_48;
+    /* 0x04C */ s32 unk_4C;
     /* 0x050 */ u8 unk_50[28];
-    /* 0x06C */ u8 unk_6c[234];
+    /* 0x06C */ u8 unk_6C[234];
     /* 0x156 */ // u16 unk_156; possibly unused space, unnecessary for match
-    /* 0x158 */ int unk_158;
-    /* 0x15C */ void* unk_15c;
+    /* 0x158 */ s32 unk_158;
+    /* 0x15C */ void* unk_15C;
 } XTrumpet;
 
-extern int D_002B8364;
-extern int D_002B8678;
+extern s32 D_002B8364;
+extern s32 D_002B8678;
 
 XTrumpet* D_002B90B0;
 
-extern int D_002B916C;
-extern int D_002B9170;
-extern int D_002B9174;
+extern s32 D_002B916C;
+extern s32 D_002B9170;
+extern s32 D_002B9174;
 
-extern int D_004D29D0;
-extern int D_004D2998;
-extern int D_004D29AC;
-extern int D_004D29C4;
+extern s32 D_004D29D0;
+extern s32 D_004D2998;
+extern s32 D_004D29AC;
+extern s32 D_004D29C4;
 
 void func_001110A0() {
     D_002B90B0 = func_0022F768(3);
 }
 
 void func_001110D0() {
-    int i;
+    s32 i;
 
     func_001110A0();
-    for (i = 0; i < 234; i++) {
-        D_002B90B0->unk_6c[i] = 0;
+    for (i = 0; i < ARRAY_COUNT(D_002B90B0->unk_6C); i++) {
+        D_002B90B0->unk_6C[i] = 0;
     }
     D_002B90B0->unk_40 = 0;
     D_002B90B0->unk_44 = 0;
     D_002B90B0->unk_10 = 0;
     D_002B90B0->unk_14 = 0;
     D_002B90B0->unk_18 = 0;
-    D_002B90B0->unk_1c = 0;
+    D_002B90B0->unk_1C = 0;
     D_002B90B0->unk_20 = 0;
     D_002B90B0->unk_24 = 0;
     D_002B90B0->unk_28 = 0;
-    D_002B90B0->unk_2c = 0;
+    D_002B90B0->unk_2C = 0;
     D_002B90B0->unk_30 = 0;
     D_002B90B0->unk_34 = 0;
     D_002B90B0->unk_38 = 0;
-    D_002B90B0->unk_3c = 0;
+    D_002B90B0->unk_3C = 0;
     D_002B90B0->unk_158 = 0;
 }
 
@@ -80,7 +80,7 @@ INCLUDE_ASM(const s32, "xtrumpet", func_00111328);
 INCLUDE_ASM(const s32, "xtrumpet", func_001113A8);
 
 void func_001113E8() {
-    func_001809C8(&D_002B90B0->unk_4c);
+    func_001809C8(&D_002B90B0->unk_4C);
 }
 
 INCLUDE_ASM(const s32, "xtrumpet", func_00111408);
@@ -89,20 +89,20 @@ INCLUDE_ASM(const s32, "xtrumpet", func_00111468);
 
 INCLUDE_ASM(const s32, "xtrumpet", func_001114B8);
 
-void func_00111580(int param1) {
+void func_00111580(s32 param1) {
     func_001114B8(D_002B8678, param1);
 }
 
 void* func_001115A0() {
-    if (D_002B90B0) {
-        return D_002B90B0->unk_15c;
+    if (D_002B90B0 != 0) {
+        return D_002B90B0->unk_15C;
     }
     return NULL;
 }
 
 void func_001115C0() {
-    if (D_002B90B0) {
-        D_002B90B0->unk_15c++;
+    if (D_002B90B0 != 0) {
+        D_002B90B0->unk_15C++;
     }
 }
 
@@ -134,7 +134,7 @@ void func_001116C8() {
 }
 
 void func_001116F8() {
-    int iVar1 = func_0013C2A8(3);
+    s32 iVar1 = func_0013C2A8(3);
     if (iVar1 != -1) {
         D_002B90B0->unk_158 |= 8;
     }
@@ -219,30 +219,32 @@ void func_00111BF0() {
     D_002B9174 = 0;
 }
 
-int func_00111C08() {
+s32 func_00111C08() {
     D_004D29AC = 2;
     return D_002B916C;
 }
 
-void func_00111C20(int param1) {
+void func_00111C20(s32 param1) {
     D_002B916C = param1 * 60;
 }
 
-void func_00111C38(int param1) {
+void func_00111C38(s32 param1) {
     D_004D29C4 = param1;
     D_002B9170 = 0;
 }
 
-void func_00111C50(int param1) {
+void func_00111C50(s32 param1) {
     D_002B9170 = param1;
 }
 
-int func_00111C60(int param1) {
-    int result;
+s32 func_00111C60(s32 param1) {
+    s32 result;
 
     D_002B9170 += param1;
-    if ((D_004D29C4 == 0) || (result = 1, D_002B9170 < D_004D29C4)) {
+    if (D_004D29C4 == 0 || D_002B9170 < D_004D29C4) {
         result = 0;
+    } else {
+        result = 1;
     }
     return result;
 }
