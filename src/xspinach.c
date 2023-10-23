6c0af1fe62e5b9ab74624c5288b4ddf3c2445548
Yotona <nzmcqueen1996@gmail.com>
2023-10-22 20:18:02 -0600

    added basic clang-format

--- src/xspinach.c
+++ src/xspinach.c
@@ -2,13 +2,15 @@
 #include "stdio.h"
 #include "string.h"
 
-const char *D_002B9178[22];
+const char* D_002B9178[22];
 //  = {
 // "dh", "di", "dc", "tw", "aw", "tz", "po", "xx", "al", "lm", "nm",
 // "he", "pi", "pp", "xx", "pc", "ew", "xx", "xx", "xx", "zz", "",
 // };
 
-char *func_00111F40(int idx, char *src) { return strcpy(src, D_002B9178[idx]); }
+char* func_00111F40(int idx, char* src) {
+    return strcpy(src, D_002B9178[idx]);
+}
 
 INCLUDE_ASM(const s32, "xspinach", func_00111F68);
 
--- src/xtrumpet.c
+++ src/xtrumpet.c
@@ -1,72 +1,70 @@
 #include "common.h"
 
-typedef struct {
-  /* 0x00 */ int unk00;
-  /* 0x04 */ int unk04;
-  /* 0x00 */ u8 unk08[8];
-  /* 0x10 */ int unk10;
-  /* 0x14 */ int unk14;
-  /* 0x18 */ int unk18;
-  /* 0x1C */ int unk1c;
-  /* 0x20 */ int unk20;
-  /* 0x24 */ int unk24;
-  /* 0x28 */ int unk28;
-  /* 0x2C */ int unk2c;
-  /* 0x30 */ int unk30;
-  /* 0x34 */ int unk34;
-  /* 0x38 */ int unk38;
-  /* 0x3C */ int unk3c;
-  /* 0x40 */ int unk40;
-  /* 0x44 */ int unk44;
-  /* 0x48 */ int unk48;
-  /* 0x4C */ int unk4c;
-  /* 0x50 */ u8 unk50[28];
-  /* 0x6C */ u8 unk6c[234];
-  /* 0x156 */ u16 unk156;
-  /* 0x158 */ int unk158;
-  /* 0x15C */ void *unk15c;
+typedef struct XTrumpet {
+    /* 0x000 */ u8 unk_00[16];
+    /* 0x010 */ int unk_10;
+    /* 0x014 */ int unk_14;
+    /* 0x018 */ int unk_18;
+    /* 0x01C */ int unk_1c;
+    /* 0x020 */ int unk_20;
+    /* 0x024 */ int unk_24;
+    /* 0x028 */ int unk_28;
+    /* 0x02C */ int unk_2c;
+    /* 0x030 */ int unk_30;
+    /* 0x034 */ int unk_34;
+    /* 0x038 */ int unk_38;
+    /* 0x03C */ int unk_3c;
+    /* 0x040 */ int unk_40;
+    /* 0x044 */ int unk_44;
+    /* 0x048 */ int unk_48;
+    /* 0x04C */ int unk_4c;
+    /* 0x050 */ u8 unk_50[28];
+    /* 0x06C */ u8 unk_6c[234];
+    /* 0x156 */ // u16 unk_156; possibly unused space, unnecessary for match
+    /* 0x158 */ int unk_158;
+    /* 0x15C */ void* unk_15c;
 } XTrumpet;
 
 extern int D_002B8364;
 extern int D_002B8678;
 
-XTrumpet *D_002B90B0;
+XTrumpet* D_002B90B0;
 
 extern int D_002B916C;
 extern int D_002B9170;
 extern int D_002B9174;
-
-// Might be part of xspinach
 
 extern int D_004D29D0;
 extern int D_004D2998;
 extern int D_004D29AC;
 extern int D_004D29C4;
 
-void func_001110A0() { D_002B90B0 = func_0022F768(3); }
+void func_001110A0() {
+    D_002B90B0 = func_0022F768(3);
+}
 
 void func_001110D0() {
-  int i;
+    int i;
 
-  func_001110A0();
-  for (i = 0; i < 234; i++) {
-    D_002B90B0->unk6c[i] = 0;
-  }
-  D_002B90B0->unk40 = 0;
-  D_002B90B0->unk44 = 0;
-  D_002B90B0->unk10 = 0;
-  D_002B90B0->unk14 = 0;
-  D_002B90B0->unk18 = 0;
-  D_002B90B0->unk1c = 0;
-  D_002B90B0->unk20 = 0;
-  D_002B90B0->unk24 = 0;
-  D_002B90B0->unk28 = 0;
-  D_002B90B0->unk2c = 0;
-  D_002B90B0->unk30 = 0;
-  D_002B90B0->unk34 = 0;
-  D_002B90B0->unk38 = 0;
-  D_002B90B0->unk3c = 0;
-  D_002B90B0->unk158 = 0;
+    func_001110A0();
+    for (i = 0; i < 234; i++) {
+        D_002B90B0->unk_6c[i] = 0;
+    }
+    D_002B90B0->unk_40 = 0;
+    D_002B90B0->unk_44 = 0;
+    D_002B90B0->unk_10 = 0;
+    D_002B90B0->unk_14 = 0;
+    D_002B90B0->unk_18 = 0;
+    D_002B90B0->unk_1c = 0;
+    D_002B90B0->unk_20 = 0;
+    D_002B90B0->unk_24 = 0;
+    D_002B90B0->unk_28 = 0;
+    D_002B90B0->unk_2c = 0;
+    D_002B90B0->unk_30 = 0;
+    D_002B90B0->unk_34 = 0;
+    D_002B90B0->unk_38 = 0;
+    D_002B90B0->unk_3c = 0;
+    D_002B90B0->unk_158 = 0;
 }
 
 INCLUDE_ASM(const s32, "xtrumpet", func_00111158);
@@ -81,7 +79,9 @@ INCLUDE_ASM(const s32, "xtrumpet", func_00111328);
 
 INCLUDE_ASM(const s32, "xtrumpet", func_001113A8);
 
-void func_001113E8() { func_001809C8(&D_002B90B0->unk4c); }
+void func_001113E8() {
+    func_001809C8(&D_002B90B0->unk_4c);
+}
 
 INCLUDE_ASM(const s32, "xtrumpet", func_00111408);
 
@@ -89,69 +89,75 @@ INCLUDE_ASM(const s32, "xtrumpet", func_00111468);
 
 INCLUDE_ASM(const s32, "xtrumpet", func_001114B8);
 
-void func_00111580(int param1) { func_001114B8(D_002B8678, param1); }
+void func_00111580(int param1) {
+    func_001114B8(D_002B8678, param1);
+}
 
-void *func_001115A0() {
-  if (D_002B90B0) {
-    return D_002B90B0->unk15c;
-  }
-  return NULL;
+void* func_001115A0() {
+    if (D_002B90B0) {
+        return D_002B90B0->unk_15c;
+    }
+    return NULL;
 }
 
 void func_001115C0() {
-  if (D_002B90B0) {
-    D_002B90B0->unk15c++;
-  }
+    if (D_002B90B0) {
+        D_002B90B0->unk_15c++;
+    }
 }
 
 void func_001115E8() {
-  D_002B8364 = 0;
-  func_0022F578(-1);
-  func_001EF840();
-  func_001601A8();
-  func_0013CB18();
-  func_001EBEB0();
-  func_001BC048();
-  func_001110D0();
+    D_002B8364 = 0;
+    func_0022F578(-1);
+    func_001EF840();
+    func_001601A8();
+    func_0013CB18();
+    func_001EBEB0();
+    func_001BC048();
+    func_001110D0();
 }
 
 INCLUDE_ASM(const s32, "xtrumpet", func_00111638);
 
-void func_001116A8() { D_004D2998 = 0; }
+void func_001116A8() {
+    D_004D2998 = 0;
+}
 
-void func_001116B8() { D_004D2998 = 1; }
+void func_001116B8() {
+    D_004D2998 = 1;
+}
 
 void func_001116C8() {
-  if (D_004D2998 == 0) {
-    func_0022F808();
-  }
+    if (D_004D2998 == 0) {
+        func_0022F808();
+    }
 }
 
 void func_001116F8() {
-  int iVar1 = func_0013C2A8(3);
-  if (iVar1 != -1) {
-    D_002B90B0->unk158 |= 8;
-  }
-  iVar1 = func_0013C2A8(5);
-  if (iVar1 != -1) {
-    D_002B90B0->unk158 |= 0x20;
-  }
-  iVar1 = func_0013C2A8(6);
-  if (iVar1 != -1) {
-    D_002B90B0->unk158 |= 0x40;
-  }
-  iVar1 = func_0013C2A8(7);
-  if (iVar1 != -1) {
-    D_002B90B0->unk158 |= 0x80;
-  }
-  iVar1 = func_0013C2A8(8);
-  if (iVar1 != -1) {
-    D_002B90B0->unk158 |= 0x100;
-  }
-  iVar1 = func_0013C2A8(9);
-  if (iVar1 != -1) {
-    D_002B90B0->unk158 |= 0x200;
-  }
+    int iVar1 = func_0013C2A8(3);
+    if (iVar1 != -1) {
+        D_002B90B0->unk_158 |= 8;
+    }
+    iVar1 = func_0013C2A8(5);
+    if (iVar1 != -1) {
+        D_002B90B0->unk_158 |= 0x20;
+    }
+    iVar1 = func_0013C2A8(6);
+    if (iVar1 != -1) {
+        D_002B90B0->unk_158 |= 0x40;
+    }
+    iVar1 = func_0013C2A8(7);
+    if (iVar1 != -1) {
+        D_002B90B0->unk_158 |= 0x80;
+    }
+    iVar1 = func_0013C2A8(8);
+    if (iVar1 != -1) {
+        D_002B90B0->unk_158 |= 0x100;
+    }
+    iVar1 = func_0013C2A8(9);
+    if (iVar1 != -1) {
+        D_002B90B0->unk_158 |= 0x200;
+    }
 }
 
 // Injecting .rodata calls, unsure how to handle this
@@ -190,7 +196,9 @@ INCLUDE_ASM(const s32, "xtrumpet", func_001117D8);
 //   }
 // }
 
-void func_00111910() { D_002B9174 = 0; }
+void func_00111910() {
+    D_002B9174 = 0;
+}
 
 INCLUDE_ASM(const s32, "xtrumpet", func_00111920);
 void func_00111920();
@@ -202,37 +210,41 @@ INCLUDE_ASM(const s32, "xtrumpet", func_00111A70);
 INCLUDE_ASM(const s32, "xtrumpet", func_00111B70);
 
 void func_00111BD8() {
-  D_004D29AC = 1;
-  D_002B9174 = 1;
+    D_004D29AC = 1;
+    D_002B9174 = 1;
 }
 
 void func_00111BF0() {
-  D_004D29AC = 0;
-  D_002B9174 = 0;
+    D_004D29AC = 0;
+    D_002B9174 = 0;
 }
 
 int func_00111C08() {
-  D_004D29AC = 2;
-  return D_002B916C;
+    D_004D29AC = 2;
+    return D_002B916C;
 }
 
-void func_00111C20(int param1) { D_002B916C = param1 * 60; }
+void func_00111C20(int param1) {
+    D_002B916C = param1 * 60;
+}
 
 void func_00111C38(int param1) {
-  D_004D29C4 = param1;
-  D_002B9170 = 0;
+    D_004D29C4 = param1;
+    D_002B9170 = 0;
 }
 
-void func_00111C50(int param1) { D_002B9170 = param1; }
+void func_00111C50(int param1) {
+    D_002B9170 = param1;
+}
 
 int func_00111C60(int param1) {
-  int result;
+    int result;
 
-  D_002B9170 += param1;
-  if ((D_004D29C4 == 0) || (result = 1, D_002B9170 < D_004D29C4)) {
-    result = 0;
-  }
-  return result;
+    D_002B9170 += param1;
+    if ((D_004D29C4 == 0) || (result = 1, D_002B9170 < D_004D29C4)) {
+        result = 0;
+    }
+    return result;
 }
 
 INCLUDE_ASM(const s32, "xtrumpet", func_00111C98);
@@ -242,15 +254,18 @@ INCLUDE_ASM(const s32, "xtrumpet", func_00111D98);
 INCLUDE_ASM(const s32, "xtrumpet", func_00111E20);
 
 void func_00111F00() {
-  D_004D29D0 = 1;
-  func_00245130();
-  func_00245140();
+    D_004D29D0 = 1;
+    func_00245130();
+    func_00245140();
 }
 
 // The following unused functions might be part of xspinach?
 
-void func_00111F28(void) {}
+void func_00111F28(void) {
+}
 
-void func_00111F30(void) {}
+void func_00111F30(void) {
+}
 
-void func_00111F38(void) {}
+void func_00111F38(void) {
+}
