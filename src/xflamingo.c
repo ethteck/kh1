d60f4b0fe8a1c8c42a7b386495afecec6dbd0436
Ethan Roseman <ethteck@gmail.com>
2023-10-29 10:49:21 +0900

    xflamingo

---
+++ src/xflamingo.c
@@ -0,0 +1,36 @@
+#include "common.h"
+
+typedef struct GiantMountain {
+    /* 0x0000 */ char unk_00[0x3070];
+    /* 0x3070 */ u64 unk_3070;
+} GiantMountain;
+
+extern GiantMountain D_003854E0;
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C36A8);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C3780);
+
+void func_001C38C0(void) {
+    D_003854E0.unk_3070 |= 0x10000000000;
+}
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C38E0);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C4000);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C4260);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C4598);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C4668);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C4740);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C4C80);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C4FF0);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C5390);
+
+INCLUDE_ASM(const s32, "xflamingo", func_001C5918);
